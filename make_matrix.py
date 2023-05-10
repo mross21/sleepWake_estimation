9f3f801c2ba4dee934871a2ab21c734719ed1b1c
mross21 <89810375+mross21@users.noreply.github.com>
2023-05-10 16:33:15 -0500

    Update make_matrix.py

--- make_matrix.py
+++ make_matrix.py
@@ -1,23 +1,4 @@
 #%%
-import pandas as pd
-import numpy as np
-
-file = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/BiAffect_data/processed_output/keypress/User_94_keypressDataMetrics.csv'
-pathOut = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/graph_regularized_SVD/'
-df = pd.read_csv(file, index_col=False)
-
-#%%
-
-df['hour'] = pd.to_datetime(df['keypressTimestampLocal']).dt.hour
-
-M = df.groupby(['dayNumber','hour'],as_index = False).size().pivot('hour','dayNumber').fillna(0)
-# rows = hour
-# columns = day number
-display(M)
-
-# M.to_csv(pathOut+'matrix.csv', index=False, header=False)
-
-# %%
 def regularized_svd(X, B, rank, alpha, as_sparse=False):
     """
     Perform graph regularized SVD as defined in
@@ -88,3 +69,84 @@ def regularized_svd(X, B, rank, alpha, as_sparse=False):
         H_star = E_tilde  # Eq 15
         W_star = E_tilde.T @ X @ inv(C)  # Eq 15
     return H_star, W_star
+
+#%%
+import pandas as pd 
+
+file = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/BiAffect_data/processed_output/keypress/User_20_keypressDataMetrics.csv'
+pathOut = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/graph_regularized_SVD/'
+
+df = pd.read_csv(file, index_col=False)
+
+df['hour'] = pd.to_datetime(df['keypressTimestampLocal']).dt.hour
+
+M = df.groupby(['dayNumber','hour'],as_index = False).size().pivot('hour','dayNumber').fillna(0)
+
+missingHours = [h for h in range(24) if h not in M.index]
+
+for h in missingHours:
+    M.loc[h] = [0]*M.shape[1]
+
+M = M.sort_index(ascending=True)
+
+# rows = hour
+# columns = day number
+display(M)
+
+#%%
+import matplotlib.pyplot as plt
+
+
+plt.imshow(M)
+plt.show()
+
+# M.to_csv(pathOut+'U20-matrix.csv', index=False, header=False)
+
+# plot singular values to get elbow for rank
+# W0 vs W1 to get sleep/wake
+
+
+
+# transform data to sigmoid or min/max scaler so it's kp vs no kp rather than continuous
+
+# try reg SVD then plot first factor or something
+
+#%%
+
+# regular SVD
+import numpy as np
+
+u, s, vh  = np.linalg.svd(M)
+print(u.shape)
+print(s.shape)
+print(vh.shape)
+
+print(s)
+# %%
+sig = (2/(1 + np.exp(-M)))-1
+cutoff = np.clip(M, 0, 100)
+
+u, s, vh  = np.linalg.svd(sig)
+print(u.shape)
+print(s.shape)
+print(vh.shape)
+
+print(s)
+# %%
+# rows = hour
+# columns = day number
+
+# hard code adjacency matrix
+W = np.zeros((M.shape[1], M.shape[1]))
+
+for i in M.shape[1]: # columns
+    for j in M.shape[0]: # rows
+        W[i,j] = M[i,j]
+
+
+
+
+
+
+
+# %%
