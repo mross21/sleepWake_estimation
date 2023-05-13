302f9b4e3f9dc34bea0c0034fc514a573882274a
mross21 <89810375+mross21@users.noreply.github.com>
2023-05-13 18:46:58 -0500

    Update make_matrix.py

--- make_matrix.py
+++ make_matrix.py
@@ -73,7 +73,7 @@ def regularized_svd(X, B, rank, alpha, as_sparse=False):
 #%%
 import pandas as pd 
 
-file = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/BiAffect_data/processed_output/keypress/User_94_keypressDataMetrics.csv'
+file = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/BiAffect_data/processed_output/keypress/User_71_keypressDataMetrics.csv'
 pathOut = '/home/mindy/Desktop/BiAffect-iOS/UnMASCK/graph_regularized_SVD/'
 
 df = pd.read_csv(file, index_col=False)
@@ -90,34 +90,171 @@ for h in missingHours:
 M = M.sort_index(ascending=True)
 M_copy = M
 
-#%%
-# REGULAR SVD
+# #%%
+# # REGULAR SVD
 
-# days = rows
-# hours = columns
+# # days = rows
+# # hours = columns
 
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-print('before')
-plt.imshow(M)
-plt.show()
-
-# sig = (2/(1 + np.exp(-M)))-1
-# cutoff = np.clip(M, 0, 100)
-# print('after transformation')
-# plt.imshow(cutoff)
+# print('before')
+# plt.imshow(M)
 # plt.show()
 
-u, s, vt  = np.linalg.svd(M)
-S = np.diag(s)
-r=1
-svd_M = u[:, :r] @ S[0:r, :r] @ vt[:r, :]
-print('after')
-plt.imshow(svd_M)
-plt.show()
+# # sig = (2/(1 + np.exp(-M)))-1
+# # cutoff = np.clip(M, 0, 100)
+# # print('after transformation')
+# # plt.imshow(cutoff)
+# # plt.show()
+
+# # u, s, vt  = np.linalg.svd(M)
+# # S = np.diag(s)
+# # r=1
+# # svd_M = u[:, :r] @ S[0:r, :r] @ vt[:r, :]
+# # print('after')
+# # plt.imshow(svd_M)
+# # plt.show()
         
-# %%
+# # %%
+
+# import numpy as np
+
+# # days = rows
+# # hours = columns
+
+# def day_weight(d1,d2):
+#     return 1#((d1+d2)/2) + 1
+# def hour_weight(h1,h2):
+#     return 1#((h1+h2)/4) + 1
+
+# M = np.array(M_copy)
+
+# # M = np.array([[1,2,3,4],
+# #              [5,6,7,8],
+# #              [9,10,11,12],
+# #              [13,14,15,16],
+# #              [17,18,19,20]])
+
+# # hard code adjacency matrix
+# W = np.zeros((M.size, M.size))
+
+# for i in range(M.size):
+#     # print('row: {}'.format(i))
+#     for j in range(M.size):
+#         # print('col: {}'.format(j))
+
+#         # iterate across hours of each day then across days
+#         # d1h1, d1h2, d1h3, d1h4...d2h1, d2h2, d3h3...
+#         i_Mi = i//M.shape[1]
+#         i_Mj = i%M.shape[1]
+#         # print('i: M[{},{}]'.format(i_Mi,i_Mj))
+#         # print('val: {}'.format(M[i_Mi,i_Mj]))
+#         j_Mi = j//M.shape[1]
+#         j_Mj = j%M.shape[1]
+#         # print('j: M[{},{}]'.format(j_Mi,j_Mj))
+#         # print('val: {}'.format(M[j_Mi,j_Mj]))
+
+#         # diagonals
+#         if i == j:
+#             W[i,j] = 0
+#             # print('diagonal')
+
+#         # # # # avoid points at boundaries
+#         # # # elif (i_Mj != M.shape[1]-1):
+
+#         # if abs(subtraction of col indices) == 1 & subtraction of row indices == 0:
+#         elif (abs(j_Mj-i_Mj) == 1) & ((j_Mi-i_Mi) == 0):
+#             W[i,j] = hour_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
+#             # print('hour connection')
+        
+#         # if abs(subtraction of row indices) == 1 & subtraction of col indices == 0:
+#         elif (abs(j_Mi-i_Mi) == 1) & ((j_Mj-i_Mj) == 0):
+#             W[i,j] = day_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
+#             # print('day connection')
+        
+#         # connect 23hr with 00hr
+#         elif (i_Mj == M.shape[1]-1) & ((j_Mi-i_Mi) == 1) & (j_Mj == 0):
+#             W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
+#             # print('boundary hour connection')
+
+#         else:
+#             W[i,j] = 0
+#             # print('no connection')
+
+#         # print('-----------------')
+            
+
+#         # # # # connect 23hr with 00hr
+#         # # # # if point is at boundary of row (23hr)
+#         # # # elif (i_Mj == M.shape[1]-1):
+            
+#         # # #     if ((j_Mj-i_Mj) == 1) & ((j_Mi-i_Mi) == 0):
+#         # # #         W[i,j] = W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
+#         # # #         print('boundary hour connection')
+
+#         # # #     if ((j_Mj-i_Mj) == -1) & ((j_Mi-i_Mi) == 0):
+#         # # #         W[i,j] = W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
+#         # # #         print('non-boundary hour connection')
+
+#         # # #     # if abs(subtraction of row indices) == 1 & subtraction of col indices == 0:
+#         # # #     elif (abs(j_Mi-i_Mi) == 1) & ((j_Mj-i_Mj) == 0):
+#         # # #         W[i,j] = day_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
+#         # # #         print('boundary day connection')
+
+#     # print('==========================================')
+
+
+        
+# print('finish with weighted adjacency matrix')
+
+# # #%%
+# # pd.DataFrame(W).to_csv(pathOut+'adj_matrix_test.csv',index=False,header=False)
+
+# #%%
+# from scipy.sparse import csgraph
+
+# # testM = np.array([[1,2,3,4],
+# #              [5,6,7,8],
+# #              [9,10,11,12],
+# #              [13,14,15,16]])
+
+# # test2=np.ones((5,5))
+
+# B = csgraph.laplacian(W)
+
+# import numpy as np
+# from scipy.linalg import svd
+# from scipy.linalg import cholesky
+# from scipy.linalg import inv
+# import scipy.sparse as sp
+# from sklearn.utils.extmath import randomized_svd
+# from sksparse import cholmod
+
+# # H_star, W_star = regularized_svd(M, B, rank=1, alpha=0.1, as_sparse=False)
+
+# alpha=0.1
+# rank=1
+
+# I = np.eye(B.shape[0])
+# C = I + (alpha * B)
+# D = cholesky(C)
+# E, S, Fh = svd(M @ inv(D.T))
+# E_tilde = E[:, :rank]  # rank-r approximation; H_star = E_tilde (Eq 15)
+# H_star = E_tilde  # Eq 15
+# W_star = E_tilde.T @ M @ inv(C)  # Eq 15
+
+
+# #%%
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+###############################################################################
+
+
+# ADJACENCY MATRIX OF SIZE DAYS X DAYS
 
 import numpy as np
 
@@ -125,9 +262,10 @@ import numpy as np
 # hours = columns
 
 def day_weight(d1,d2):
-    return 1#((d1+d2)/2) + 1
-def hour_weight(h1,h2):
-    return 1#((h1+h2)/4) + 1
+    # calculate how the pattern that day compares with all other days
+    return np.sqrt(np.sum((d1 - d2) ** 2))
+# def hour_weight(h1,h2):
+#     return 1 #((h1+h2)/4) + 1
 
 M = np.array(M_copy)
 
@@ -135,84 +273,59 @@ M = np.array(M_copy)
 #              [5,6,7,8],
 #              [9,10,11,12],
 #              [13,14,15,16],
-#              [17,18,19,20]])
+#              [17,18,19,20],
+#              [21,22,23,24],
+#              [25,26,27,28]])
 
 # hard code adjacency matrix
-W = np.zeros((M.size, M.size))
+W = np.zeros((M.shape[0], M.shape[0]))
 
-for i in range(M.size):
-    # print('row: {}'.format(i))
-    for j in range(M.size):
-        # print('col: {}'.format(j))
+for i in range(M.shape[0]):
+    print('row: {}'.format(i))
+    for j in range(M.shape[0]):
+        print('col: {}'.format(j))
 
-        # iterate across hours of each day then across days
-        # d1h1, d1h2, d1h3, d1h4...d2h1, d2h2, d3h3...
-        i_Mi = i//M.shape[1]
-        i_Mj = i%M.shape[1]
+        # # iterate across hours of each day then across days
+        # # d1h1, d1h2, d1h3, d1h4...d2h1, d2h2, d3h3...
+        # i_Mi = i//M.shape[1]
+        # i_Mj = i%M.shape[1]
         # print('i: M[{},{}]'.format(i_Mi,i_Mj))
         # print('val: {}'.format(M[i_Mi,i_Mj]))
-        j_Mi = j//M.shape[1]
-        j_Mj = j%M.shape[1]
+        # j_Mi = j//M.shape[1]
+        # j_Mj = j%M.shape[1]
         # print('j: M[{},{}]'.format(j_Mi,j_Mj))
         # print('val: {}'.format(M[j_Mi,j_Mj]))
 
         # diagonals
         if i == j:
             W[i,j] = 0
-            # print('diagonal')
-
-        # # # # avoid points at boundaries
-        # # # elif (i_Mj != M.shape[1]-1):
-
-        # if abs(subtraction of col indices) == 1 & subtraction of row indices == 0:
-        elif (abs(j_Mj-i_Mj) == 1) & ((j_Mi-i_Mi) == 0):
-            W[i,j] = hour_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
-            # print('hour connection')
+            print('diagonal')
         
-        # if abs(subtraction of row indices) == 1 & subtraction of col indices == 0:
-        elif (abs(j_Mi-i_Mi) == 1) & ((j_Mj-i_Mj) == 0):
-            W[i,j] = day_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
-            # print('day connection')
-        
-        # connect 23hr with 00hr
-        elif (i_Mj == M.shape[1]-1) & ((j_Mi-i_Mi) == 1) & (j_Mj == 0):
-            W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
-            # print('boundary hour connection')
+        # days
+        elif abs(i-j) == 1:
+            print('day connection')
+            # if i >= M.shape[1]:
+            #     print('bad i')
+            #     print('+++++++++++++++++++++++++++++++++++++++++++++++++')
+            # elif j >= M.shape[1]:
+            #     print('bad j')
+            #     print('+++++++++++++++++++++++++++++++++++++++++++++++++')
+            # else:
+            W[i,j] = day_weight(M[i,:], M[j,:])
 
         else:
             W[i,j] = 0
-            # print('no connection')
+            print('no connection')
 
-        # print('-----------------')
-            
+        print('-----------------')
 
-        # # # # connect 23hr with 00hr
-        # # # # if point is at boundary of row (23hr)
-        # # # elif (i_Mj == M.shape[1]-1):
-            
-        # # #     if ((j_Mj-i_Mj) == 1) & ((j_Mi-i_Mi) == 0):
-        # # #         W[i,j] = W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
-        # # #         print('boundary hour connection')
-
-        # # #     if ((j_Mj-i_Mj) == -1) & ((j_Mi-i_Mi) == 0):
-        # # #         W[i,j] = W[i,j] = hour_weight(M[i_Mi,i_Mj],M[i_Mi+1,0])
-        # # #         print('non-boundary hour connection')
-
-        # # #     # if abs(subtraction of row indices) == 1 & subtraction of col indices == 0:
-        # # #     elif (abs(j_Mi-i_Mi) == 1) & ((j_Mj-i_Mj) == 0):
-        # # #         W[i,j] = day_weight(M[i_Mi,i_Mj],M[j_Mi,j_Mj])
-        # # #         print('boundary day connection')
-
-    # print('==========================================')
-
+    print('==========================================')
 
         
 print('finish with weighted adjacency matrix')
 
 # #%%
-# pd.DataFrame(W).to_csv(pathOut+'adj_matrix_test.csv',index=False,header=False)
 
-#%%
 from scipy.sparse import csgraph
 
 # testM = np.array([[1,2,3,4],
@@ -223,8 +336,7 @@ from scipy.sparse import csgraph
 # test2=np.ones((5,5))
 
 B = csgraph.laplacian(W)
-
-#%%
+M = M_copy.T
 import numpy as np
 from scipy.linalg import svd
 from scipy.linalg import cholesky
@@ -235,8 +347,8 @@ from sksparse import cholmod
 
 # H_star, W_star = regularized_svd(M, B, rank=1, alpha=0.1, as_sparse=False)
 
-alpha=0.1
-rank=1
+alpha = 0.1
+rank = 1
 
 I = np.eye(B.shape[0])
 C = I + (alpha * B)
@@ -248,25 +360,50 @@ W_star = E_tilde.T @ M @ inv(C)  # Eq 15
 
 
 
+plt.imshow(M_copy)
+plt.show()
 
-# %%
-# # days = rows
-# # hours = columns
 
-# def day_weight(d1,d2):
-#     return (d1+d2)/2
-# def hour_weight(h1,h2):
-#     return (h1+h2)/4
+out=H_star@W_star
+out = out.T
+plt.imshow(out)
+plt.show()
 
-# M = np.array(M_copy)[13:20,13:17]
+
+
+
+
+# cutoff = np.clip(out, 0, )
+
+# plt.imshow(cutoff,vmin=0, vmax=20, aspect='auto')
+# plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#%%
+
+# ## COPY
 
 # # hard code adjacency matrix
-# W = np.zeros((M.size, M.size))
+# W = np.zeros((M.shape[0], M.shape[0]))
 
-# for i in range(M.size): # rows = days
+# for i in range(M.shape[0]): # rows = days
 #     print('row: {}'.format(i))
 
-#     for j in range(M.size): # columns = hours
+#     for j in range(M.shape[0]): # columns = hours
 #         print('col: {}'.format(j))
 #         # loop through hours within day
 #         # connect hours within one day(inside borders)
